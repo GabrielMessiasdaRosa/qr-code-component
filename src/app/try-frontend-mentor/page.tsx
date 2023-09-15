bfb2cc342f505234018796a343833791fbebd629
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-09-15 01:54:45 -0300

    feat: added try page and implement qr code component

---
+++ src/app/try-frontend-mentor/page.tsx
@@ -0,0 +1,9 @@
+import QrCodeToFrontendMentorCard from "@/components/qr-code-to-frontend-mentor-card";
+
+export default function TryFrontendMentor() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <QrCodeToFrontendMentorCard />
+    </main>
+  );
+}
